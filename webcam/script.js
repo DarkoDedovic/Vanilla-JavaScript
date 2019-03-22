6889533e35102ec7d50c20c38eddb956b2966d72
Darko Dedovic <milos.metlic@42coders.com>
2019-03-22 14:12:20 +0100

    webCam finished

--- webcam/script.js
+++ webcam/script.js
@@ -29,10 +29,10 @@ function paintToCanvas() {
   return setInterval(() => {
     ctx.drawImage(video, 0, 0, width, height);
     let pixels = ctx.getImageData(0, 0, width, height);
-    console.log(pixels);
-    pixels = redEffect(pixels);
+    // pixels = redEffect(pixels);
+    // pixels = rgbSplit(pixels);
+    pixels = greenScreen(pixels);
     ctx.putImageData(pixels, 0, 0);
-    // debugger;
   }, 16);
 }
 
@@ -57,6 +57,46 @@ function redEffect(pixels) {
     pixels.data[i + 2] = pixels.data[i + 2] * 0.5; //blue
   }
   return pixels;
+}
+
+function rgbSplit(pixels) {
+
+  for (let i = 0; i < pixels.data.length; i += 4) {
+    pixels.data[i - 150] = pixels.data[i + 1]; // red
+    pixels.data[i + 500] = pixels.data[i + 1]; // green
+    pixels.data[i - 550] = pixels.data[i + 2]; //blue
+  }
+  return pixels;
+}
+
+function greenScreen(pixels) {
+  let inputs = document.querySelectorAll(".rgb input");
+  let levels = {};
+  console.log(inputs);
+  inputs.forEach(input => {
+    levels[input.name] = input.value;
+  })
+  console.log(pixels.data);
+
+  for (i = 0; i < pixels.data.length; i = i + 4) {
+    red = pixels.data[i + 0];
+    blue = pixels.data[i + 1];
+    green = pixels.data[i + 2];
+    alpha = pixels.data[i + 3];
+debugger;
+    if (red >= levels.rmin &&
+      green >= levels.gmin &&
+      blue >= levels.bmin &&
+      red <= levels.rmax &&
+      green <= levels.gmax &&
+      blue <= levels.bmax) {
+      // take it out 
+      pixels.data[i + 3] = 0;
+    }
+  }
+  console.log(levels.bmax);
+  return pixels;
+
 }
 
 getVideo();
